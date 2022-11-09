fa59b6a1a14a9e37a720c8e79766fccc6a16345e
Jiabao Sun <jiabao.sun@xtransfer.cn>
2022-11-10 01:42:16 +0800

    Add javadocs about _id extract logic

--- flink-connector-mongodb/src/main/java/org/apache/flink/connector/mongodb/common/utils/MongoValidationUtils.java
+++ flink-connector-mongodb/src/main/java/org/apache/flink/connector/mongodb/common/utils/MongoValidationUtils.java
@@ -18,11 +18,15 @@
 package org.apache.flink.connector.mongodb.common.utils;
 
 import org.apache.flink.annotation.Internal;
+import org.apache.flink.connector.mongodb.table.MongoKeyExtractor;
+import org.apache.flink.connector.mongodb.table.converter.RowDataToBsonConverters;
 import org.apache.flink.table.api.ValidationException;
 import org.apache.flink.table.types.DataType;
 import org.apache.flink.table.types.logical.LogicalType;
 import org.apache.flink.table.types.logical.LogicalTypeFamily;
 import org.apache.flink.table.types.logical.LogicalTypeRoot;
+
+import org.bson.BsonType;
 
 import java.util.EnumSet;
 import java.util.List;
@@ -51,11 +55,37 @@ public class MongoValidationUtils {
 
     /**
      * Checks that the table does not have a primary key defined on illegal types. In MongoDB the
-     * primary key is used to calculate the MongoDB document id, which is a string of up to 1024
-     * bytes. It cannot have whitespaces. As of now it is calculated by concatenating the fields.
-     * Certain types do not have a good string representation to be used in this scenario. The
-     * illegal types are mostly {@link LogicalTypeFamily#COLLECTION} types and {@link
-     * LogicalTypeRoot#RAW} type.
+     * primary key is used to calculate the MongoDB document _id, which may be of any {@link
+     * BsonType} other than a {@link BsonType#ARRAY}. Its value must be unique and immutable in the
+     * collection.
+     *
+     * <p>MongoDB creates a unique index on the _id field during the creation of a collection. There
+     * are also some constraints on the primary key index. For more detailed introduction, you can
+     * refer to <a
+     * href="https://www.mongodb.com/docs/manual/reference/limits/#mongodb-limit-Index-Key-Limit">
+     * Index Key Limit</a>.
+     *
+     * <ul>
+     *   <li>Before MongoDB 4.2, the total size of an index entry, which can include structural
+     *       overhead depending on the BSON type, must be less than 1024 bytes.
+     *   <li>Starting in version 4.2, MongoDB removes the Index Key Limit.
+     * </ul>
+     *
+     * <p>As of now it is extracted by {@link MongoKeyExtractor} according to the primary key
+     * specified by the Flink table schema.
+     *
+     * <ul>
+     *   <li>When there's only a single field in the specified primary key, we convert the field
+     *       data to bson value as _id of the corresponding document.
+     *   <li>When there's multiple fields in the specified primary key, we convert and composite
+     *       these fields into a {@link BsonType#DOCUMENT} as the _id of the corresponding document.
+     *       For example, if have a primary key statement <code>PRIMARY KEY (f1, f2) NOT ENFORCED
+     *       </code>, the extracted _id will be the form like <code>_id: {f1: v1, f2: v2}</code>
+     * </ul>
+     *
+     * <p>The illegal types are mostly {@link LogicalTypeFamily#COLLECTION} types and {@link
+     * LogicalTypeRoot#RAW} type and other types that cannot be converted to {@link BsonType} by
+     * {@link RowDataToBsonConverters}.
      */
     public static void validatePrimaryKey(DataType primaryKeyDataType) {
         List<DataType> fieldDataTypes = DataType.getFieldDataTypes(primaryKeyDataType);
