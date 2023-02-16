33f9d6a1f607e7492122a6ac5a6a280f256b4b5f
Jiabao Sun <jiabao.sun@xtransfer.cn>
2023-02-16 22:58:02 +0800

    Remove unnecessary parameter

--- flink-connector-mongodb/src/test/java/org/apache/flink/connector/mongodb/source/enumerator/MongoSourceEnumStateSerializerTest.java
+++ flink-connector-mongodb/src/test/java/org/apache/flink/connector/mongodb/source/enumerator/MongoSourceEnumStateSerializerTest.java
@@ -76,7 +76,6 @@ public class MongoSourceEnumStateSerializerTest {
                 "coll",
                 new BsonDocument("_id", new BsonInt32(index)),
                 new BsonDocument("_id", MongoConstants.BSON_MAX_KEY),
-                ID_HINT,
-                index);
+                ID_HINT);
     }
 }
